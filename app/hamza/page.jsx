548585fe427f23ba376ab754828aef3ef5d1fc11
HamzaMurghay <hamzamurghay@gmail.com>
2024-02-10 23:15:02 +0530

    new task description

--- app/hamza/page.jsx
+++ app/hamza/page.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-        <nav class="navbar">
-            <ul className="flex-col-reverse gap-3">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Tasks</a></li>
-                <li><a href="#">Calendar</a></li>
-                <li><a href="#">Shop</a></li>
+    // Task description
+    <div className="w-72 h-52 bg-white bg-opacity-5 rounded-xl">
+      <div className="w-full h-8 border-util border-b-[1px] border-opacity-20 text-white pl-8 py-1">Title of Task</div>
+      <div className="pt-2 pl-6 text-slate-400">Details of the task go here</div>
+      <div className="flex pt-10 pl-6  text-slate-400 gap-4">
+        <div className=" border-util border-2 px-4">Calendar</div>
+        <div className="pl-5 border-util border-2 px-4">Delete</div>
+      </div>
 
-            </ul>
-        </nav>
+      <div className="flex text-slate-400 gap-3 pl-6 pt-5">
+        <div  className="border-2 border-util px-2">Urgent</div>
+        <div className="border-2 border-util px-2">Leisure</div>
+      </div>
     </div>
   )
 }
