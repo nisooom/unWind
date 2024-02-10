aa9adaebb522c042e781c81e744518b69172bceb
Pranavi <pranavi.shukla@gmail.com>
2024-02-10 15:56:30 +0530

    page

--- app/pranavi/page.jsx
+++ app/pranavi/page.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import Sidebar from '@/components/Sidebar'
+import { Checkbox } from '@/components/ui/checkbox'
+
 const page = () => {
   return (
-    <div className="w-full h-full bg-blue-800">
+    <div className="w-full h-full bg-black">
+       <div className="items-top flex space-x-2">
+      <Checkbox id="terms1" />
+      <div className="grid gap-1.5 leading-none">
+        <label
+          className="text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 bg-white"
+        >
+          Complete Assignmnet
+        </label>
+        <p className="text-sm text-muted-foreground">
+          You agree to our Terms of Service and Privacy Policy.
+        </p>
+      </div>
+    </div>
   
-        <Sidebar />
+      
 
   </div>
   )
