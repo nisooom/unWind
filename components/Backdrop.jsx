0c6ebb8ed012ff99022d9097b6806a9fa8ae9000
TejasBhovad <tejasbhovad@gmail.com>
2024-02-10 14:43:02 +0530

    randokm

---
+++ components/Backdrop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Backdrop = () => {
+  return (
+    <div className="text-white pl-40 w-full h-full absolute text-9xl">
+      Backdrop
+    </div>
+  );
+};
+
+export default Backdrop;
