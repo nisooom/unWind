f3d6b80c10c0e41d219386481e15e09dd8c44cf1
nisooom <shahnishumbh55@gmail.com>
2024-02-11 00:37:28 +0530

    datepicker fixed

--- app/hamza/page.jsx
+++ app/hamza/page.jsx
@@ -1,10 +1,5 @@
 "use client"
 
-// import React from 'react'
-// import useState from 'react'
-import { CalendarIcon } from "@radix-ui/react-icons"
-import { format } from "date-fns"
- 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
@@ -13,38 +8,9 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
-
-// export function pickDate(){
-//   const [date, setDate] = useState(new Date())
-//   return (
-//     <Popover>
-//     <PopoverTrigger asChild>
-//         <Button
-//           variant={"outline"}
-//           className={cn(
-//             "w-[240px] justify-start text-left font-normal",
-//             !date && "text-muted-foreground"
-//           )}
-//         >
-//           <CalendarIcon className="mr-2 h-4 w-4" />
-//           {date ? format(date, "PPP") : <span>Pick a date</span>}
-//         </Button>
-//       </PopoverTrigger>
-//       <PopoverContent className="w-auto p-0" align="start">
-//         <Calendar
-//           mode="single"
-//           selected={date}
-//           onSelect={setDate}
-//           initialFocus
-//         />
-//       </PopoverContent>
-//     </Popover>
-//   )
-// }
-
-import React, { useState } from "react";
+import { useState } from "react";
 import DatePicker from "react-datepicker";
-
+import "react-datepicker/dist/react-datepicker.css";
 
 function page() {
   const [date, setDate] = useState(new Date());
@@ -55,10 +21,8 @@ function page() {
       <div className="w-full h-8 border-util border-b-[1px] border-opacity-20 text-white pl-8 py-1">Title of Task</div>
       <div className="pt-2 pl-6 text-slate-400">Details of the task go here</div>
       <div className="flex pt-10 pl-6  text-slate-400 gap-4">
-        <button className="border-util border-2 px-4">Calendar
-        </button>
-        <div>
-      <DatePicker selected={date} onChange={(date) => setDate(date) } className="text-white border-util border-2"/>
+      <DatePicker selected={date} onChange={(date) => setDate(date) } className="w-28 justify-center items-center bg-white bg-opacity-20 text-center border-util border-1 rounded-xl h-6"/>  
+      <div>
       </div>
         <div className="pl-5 border-util border-2 px-4">Delete</div>
       </div>
