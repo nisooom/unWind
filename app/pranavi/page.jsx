c8311ac6eec5e870715f7e00260f2f1c16306d3e
Pranavi <pranavi.shukla@gmail.com>
2024-02-10 13:55:06 +0530

    Co-authored-by: Tejas Bhovad <TejasBhovad@users.noreply.github.com>

---
+++ app/pranavi/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '@/components/Sidebar'
+const page = () => {
+  return (
+    <div className="w-full h-full bg-blue-800">
+  
+        <Sidebar />
+
+  </div>
+  )
+}
+
+export default page
---
+++ components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='w-40 h-full bg-white'>Sidebar</div>
+  )
+}
+
+export default Sidebar
