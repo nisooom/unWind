e6ab84072410e4cba15a20cf477d4c9377891a27
HamzaMurghay <hamzamurghay@gmail.com>
2024-02-10 16:37:22 +0530

    Update User data

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,16 +15,10 @@ export default function RootLayout({ children, session }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="flex h-full">
-<<<<<<< HEAD
-          <Sidebar /> 
-          <Backdrop />
-          <main className="pl-40 flex bg-black box-border w-full">
-            <div className="w-full h-full z-20"> {children}</div>
-=======
+
           <Sidebar /> <Backdrop />
           <main className="pl-56 flex bg-black box-border w-full">
             <div className="w-full h-full z-20 px-4"> {children}</div>
->>>>>>> d2313919784a865456247f64d64cee3e015730ed
           </main>
         </div>
       </body>
--- db/update-user-data.js
+++ db/update-user-data.js
@@ -3,10 +3,12 @@ import {supabase} from "@/supabase";
 export default async function updateUserData() {
     console.log("OMGDATAupdateFROMHERE");
     const { data, error } = await supabase.from("users").update(
-        [
-            { name: "John", email: "kdahfjh@fdad", coins: 100, exp: 100 }
-        ]
-    );
+        { 
+            name: "JACK", email: "kdahfjh@fdad", coins: 100, exp: 100 
+        }
+    ).eq(
+        "name","John"
+        );
     if (error) throw error;
     console.log(data);
     return data;
